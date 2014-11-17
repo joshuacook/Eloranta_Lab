74a589b2cccc65e8cfff827edc277b18acc440db
Joshua Cook <me@joshuacook.me>
2014-11-17 12:22:59 -0800

    Test opebBLAS and create makefile

---
+++ src/test_cblas_open.c
@@ -0,0 +1,22 @@
+#include <cblas.h>
+#include <stdio.h>
+
+int main()
+{
+  int i=0;
+  double A[6] = {1.0,2.0,1.0,-3.0,4.0,-1.0};         
+  double B[6] = {1.0,2.0,1.0,-3.0,4.0,-1.0};  
+  double C[9] = {.5,.5,.5,.5,.5,.5,.5,.5,.5}; 
+  cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans,3,3,2,1,A, 3, B, 3,2,C,3);
+
+  for(i=0; i<9; i++)
+    printf("%lf ", C[i]);
+  printf("\n");
+  
+  // test 
+  
+  return 0;
+}
+
+
+// gcc -o test_cblas_open test_cblas_open.c -I ~/bin/OpenBLAS/ -L ~/bin/OpenBLAS/ -lopenblas -lpthread -lgfortran

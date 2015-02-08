8c249974ec2c633cf1c87bdfca18c7141fbd8a04
Joshua Cook <me@joshuacook.me>
2015-02-07 18:55:00 -0800

    updated blas_utilities.h with pointer arithmetic toward column major

--- toward_dft/src/ax_equals_b.c
+++ toward_dft/src/ax_equals_b.c
@@ -9,24 +9,19 @@
 #include <math.h>
 #include "/usr/local/Cellar/openblas/0.2.12/include/cblas.h"
 #include "blas_fortran_double.h"
+#include "blas_utilities.h"
 
-void print_matrix(double * matrix, int n);
 
 int main(int argc, char* argv[]){
   printf("Factor a Matrix into its upper triangular portion\n");
 
   int n = 3; 
 
-  double A[n*n] = { 2, 1, 3, -3, 4, 7, -12, -5, 3};                
-  // matrix to be examined
-  // note that this matrix is in column major order and represents the following matrix
-  //     2  -3  -12
-  // A=  1   4   -5
-  //     3   7    3
+  double A[n*n];                // initial matrix
+ 
+  double U[n*n];                // to hold factored matrix
 
-  double U[n*n] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};                // to hold factored matrix
-
-  print_matrix(A);
+  print_matrix(A, n, n);
 
   for (int col = 0; col < n; col++){
     *(U + col*n) = *(A + col*n);
@@ -36,19 +31,12 @@ int main(int argc, char* argv[]){
 
   }
 
-  print_matrix(U);
+  print_matrix(U, n, n);
 
   return 0;
 
 }
 
-void print_matrix(double * matrix, int n){
-  printf("\n");
-  for (int col = 0; col < n; col++){
-    for (int row = 0; row < n; row++){
-      printf("\t%f",*(matrix+row*3+col)); // *(matrix+row*3+col) used to travers column major matrix
-    }
-    printf("\n");
-  }
-  printf("\n");
-}
+
+
+
--- toward_dft/src/blas_utilities.h
+++ toward_dft/src/blas_utilities.h
@@ -1,14 +1,17 @@
 /* This header contains multiple utility functions used in working within the BLAS framework */ 
 /* Stylistically, BLAS functions typically take pointers as input. These functions are taking the variables themselves. */
 
+#include <time.h>
+
 #define NEWLINE printf("\n");
 
+
 // Function Declarations
+void random_matrix(double * A, int n, int m);
 void print_matrix(double * A, int n, int m);
 void print_vector(double * A, int n);
-void random_matrix(double * A, int n, int m);
-void random_vector(double *A, int n);
-void identity_matrix(double * I, int n);
+void random_vector(double * A, int n);
+void identity_matrix(double * eye, int n);
 
 // Matrix and Vector Generators for testing
 void random_matrix(double * A, int m, int n){
@@ -21,6 +24,7 @@ void random_matrix(double * A, int m, int n){
     *(A + i) = drand48();
   }
 }
+
 void random_vector(double * A, int n){
   time_t timer;
   int i; 
@@ -31,11 +35,12 @@ void random_vector(double * A, int n){
     *(A + i) = drand48();
   } 
 } 
-void identity_matrix(double * I, int n){
+
+void identity_matrix(double * eye, int n){
   int i;
   
   for(i = 0; i < n + 1; i++){
-    *(I + i*(n+1)) = 1;
+    *(eye + i*(n+1)) = 1;
   } 
 } 
 
@@ -44,9 +49,9 @@ void print_vector(double * A, int n){
   int i;
   printf("(");
   for (i = 0; i < n-1; i++){
-    printf("%f, ", *(A + i);
+    printf("%f, ", *(A + i));
   }
-  printf("%f", *(A + i);
+  printf("%f", *(A + i));
   printf(")");
   NEWLINE;
   NEWLINE;
@@ -55,7 +60,7 @@ void print_matrix(double * A, int n, int m){
   int i,j;
   for (int col = 0; col < n; col++){
     for (int row = 0; row < m; row++){
-      printf("\t%f",*(matrix+row*n+col)); // *(matrix+row*3+col) used to travers column major matrix
+      printf("\t%f",*(A + row*n + col)); // *(matrix+row*3+col) used to travers column major matrix
     } 
     NEWLINE;
   } 

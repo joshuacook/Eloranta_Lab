2a43895964b4898ee5c147a47aaff564ea341ac3
Joshua Cook <me@joshuacook.me>
2015-02-07 17:20:00 -0800

    generalized for matrix of size n

--- toward_dft/src/ax_equals_b.c
+++ toward_dft/src/ax_equals_b.c
@@ -15,25 +15,36 @@ void print_matrix(double * matrix);
 int main(int argc, char* argv[]){
   printf("Factor a Matrix into its upper triangular portion\n");
 
-  double A[3*3] = { 2, 1, 3, -3, 4, 7, -12, -5, 3};                // matrix to be examined
+  int n = 3; 
+
+  double A[n*n] = { 2, 1, 3, -3, 4, 7, -12, -5, 3};                
+  // matrix to be examined
   // note that this matrix is in column major order and represents the following matrix
   //     2  -3  -12
   // A=  1   4   -5
   //     3   7    3
-  
-  double U[3*3] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};                // too hold factored matrix
+
+  double U[n*n] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};                // to hold factored matrix
 
   print_matrix(A);
+
+  for (int i = 0; i < n; i++){
+    *(U + i*n) = *(A + i*n);
+  }
+
+  print_matrix(U);
 
   return 0;
 
 }
 
-void print_matrix(double * matrix){
-  for (int i = 0; i < 3; i++){
-    for (int j = 0; j < 3; j++){
-      printf("\t%f",*(matrix+j*3+i));
+void print_matrix(double * matrix, n){
+  printf("\n");
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < n; j++){
+      printf("\t%f",*(matrix+j*n+i));
     }
     printf("\n");
   }
+  printf("\n");
 }

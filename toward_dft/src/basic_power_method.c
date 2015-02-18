38e1f6f9f9d531217e24ce86a6002c3fa39fc288
Joshua Cook <me@joshuacook.me>
2015-02-18 10:37:00 -0800

    implementing a stopping criteria but not quite working correctly

--- toward_dft/src/basic_power_method.c
+++ toward_dft/src/basic_power_method.c
@@ -17,16 +17,19 @@
 
 int main(int argc, char* argv[])
 {
+  float eps = 10e-6;
   int i;
   printf("A simple power method iteration to find largest eigenvector\n");
 
   int n = 4;
 
   double A[n*n];                 // matrix to be examined
+  double A_2[n*n];
 
   random_matrix(A, n, n, 1234);
   print_matrix(A, n, n);
-     
+
+
   double alpha = 1.0;           // scaling factor
   double beta = 1.0;            // scaling factor 
   double mu;                    // norm
@@ -35,6 +38,10 @@ int main(int argc, char* argv[])
   int one = 1;                  // fortran requires pointers
   int two = 2;
   char no_trans='N';                    
+  float conv = 1.0;
+
+  // Calculate A^2
+  dgemm_(&no_trans, &no_trans, &n, &n, &n, &alpha, A, &n, A, &n, &alpha, A_2, &n);
 
   // Initialize and display a random vector 
   double x[n] ; 
@@ -43,8 +50,10 @@ int main(int argc, char* argv[])
   
   // Will hold resulting vector
   double y[n] ;
+  double temp[n];
+  float var_1, var_2;
 
-  for (i = 0; i < 20; i++){
+  while (conv > eps){
     // Perform matrix multiplication
     // row_order  transform lenY lenX alpha  a  lda  X  incX  beta  Y, incY 
     // y := alpha*A*x + beta*y
@@ -71,6 +80,17 @@ int main(int argc, char* argv[])
     // Copy into x for next iteration  
 
     dcopy_(&n, y,&one, x,&one);
+    
+    // stopping criteria
+    dgemv_(&no_trans, &n, &n, &alpha, A_2, &n, y, &one, &beta, temp, &one);
+    var_1 = ddot_ (&n, temp, &one, y, &one);
+    printf("var_1: %f\n", var_1 );
+    dgemv_(&no_trans, &n, &n, &alpha, A, &n, y, &one, &beta, temp, &one);
+    var_2 = ddot_ (&n, temp, &one, y, &one);
+    var_2 = var_2*var_2;
+    printf("var_2: %f\n", var_2 );
+    conv = sqrt(2)*sqrt(abs(var_1-var_2));
+    printf("%f\n", conv );
   }
 
   return 0;

f369efc34ad0415165397d3d0d6b1e4d723fc90c
Joshua Cook <me@joshuacook.me>
2015-02-19 09:48:00 -0800

    building the same in numpy; need to align expectations in the two, particularly while python will be used to check the accuracy of results

---
+++ toward_dft/py/basic_power_method.py
@@ -0,0 +1,23 @@
+from numpy import *
+
+class blas():
+    def main(self):     
+        print("A simple power method iteration to find largest eigenvector.")
+
+    def random_matrix(rows, cols):
+      return 
+
+    def print_matrix(rows, cols):
+      return
+
+    def print_vector(n):
+      return
+
+    def random_vector(n):
+      return
+
+    def identity_matrix(n):
+      return
+
+if __name__ == '__main__':
+    blas().main()
